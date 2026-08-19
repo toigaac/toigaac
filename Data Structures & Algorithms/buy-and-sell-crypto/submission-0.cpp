b4fb807dc220f7a26a3bd7ca18290cd39e47af7b
toigaac <nghiapm007@gmail.com>
2026-08-19 14:53:16 +0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0,r=1;
+        int ans=0;
+        while(r<prices.size()){
+            if(prices[l]<prices[r]){
+                int profit=prices[r]-prices[l];
+                ans=max(ans,profit);
+            }
+            else{
+                l=r;
+            }
+            r++;
+
+        }
+        return ans;
+    }
+};
